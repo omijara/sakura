79bc4c888d79c342734acf52ef169bb271924855
Liton Mazumder <31031569+omijara@users.noreply.github.com>
2022-10-26 12:08:09 +0600

    Update epiz_32786088_sakura.sql

--- layouts/db/epiz_32786088_sakura.sql
+++ layouts/db/epiz_32786088_sakura.sql
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `epiz_32786088_sakura`
+-- Database: `sakura_inv`
 --
 
 -- --------------------------------------------------------
